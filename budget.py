3026590f8c7a9cfac2fc44f9fb19791c613c539d
Leis Al Ayoubi <77484094+LeisAlAyoubi@users.noreply.github.com>
2021-07-07 08:06:57 +0000

    INIT; added deposit method

--- budget.py
+++ budget.py
@@ -1,7 +1,28 @@
 class Category:
+  category_name = ""
+  ledger = list()
+
+  def __init__(self,name):
+    self.category_name = name
+  
+  def deposit(self, amount, description=""):
+    deposit_dict = dict()
+    deposit_dict["amount" ] = amount
+    deposit_dict["description" ] = description
+    self.ledger.append(deposit_dict)
+  
+  #def withdraw(self, amount, description=""):
+    
+
+
+
+
+
+
 
 
 
 
 
 def create_spend_chart(categories):
+  pass
